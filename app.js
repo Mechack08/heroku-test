e7d72ef88e3e720433d85c683e52dbf70582664c
8pro <mechackkobius@gmail.com>
2022-07-13 15:49:57 +0300

    register

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.send("Login Page !");
 });
+app.get("/register", (req, res) => {
+  res.send("Registration Page !");
+});
 
 app.listen(process.env.PORT || 3002, () => {
   console.log("Server on : http://localhost:3002");
